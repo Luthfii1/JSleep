82d3d533f03372f74ae561a97e1e522a107b91f2
Luthfii1 <luthfi.misbachul@ui.ac.id>
2022-09-13 17:49:11 +0700

    Seluruh method telah berjalan

--- JSleep/LuthfiMisbachulMunirJSleepFN/JSleep.java
+++ JSleep/LuthfiMisbachulMunirJSleepFN/JSleep.java
@@ -1,35 +1,77 @@
 package LuthfiMisbachulMunirJSleepFN;
 
+import java.util.*;
+import java.lang.Math;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-/**
- * Write a description of class JSleep here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class JSleep
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class JSleep
-     */
-    public JSleep()
-    {
-        // initialise instance variables
-        x = 0;
+public class JSleep {
+    public static void main (String[] args){
+        getHotelId();
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    //This method return 0
+    static int getHotelId (){
+        return 0;
+    }
+    
+    //This method return "hotel"
+    static String getHotelName (){
+        return "hotel";
+    }
+    
+    //This method return true
+    static boolean isDiscount (int num){
+        return true;
+    }
+    
+    //This method return persentase dari potongan harga
+    static float getDiscountPercentage (int beforeDiscount, int afterDiscount){
+        float harga, persen;
+        
+        if (beforeDiscount > afterDiscount){
+            harga = beforeDiscount - afterDiscount;
+            persen = (harga/beforeDiscount)*100;
+        } else {
+            persen = (float) 0.0;
+        }
+
+        return persen;
+    }
+    
+    //This method return int
+    static int getDiscountPrice (int price, float discountPercentage){
+        int hargaAkhir, potongan;
+        
+        if(discountPercentage > 100){
+            hargaAkhir = 0;
+        } else {
+            potongan = (price * (int)discountPercentage)/100;
+            hargaAkhir = price - potongan;
+        }
+        
+        return hargaAkhir;
+    }
+
+    //BANG GUA BARU MULAI DARI JAM 16.38 TOLONG
+    static int getOriginalPrice (int discountPrice, float discountPercentage){
+        return (discountPrice*100)/(100 - (int) discountPercentage);
+    }
+
+    //This method return 0
+    static float getAdminFeePercentage (){
+        return (float) 5/100;
+    }
+
+    static int getAdminFee (int price){
+        float pinjem;
+        
+        pinjem = getAdminFeePercentage()*100;
+        return price * (int)pinjem/100;
+    }
+
+    static int getTotalPrice (int price, int numberOfNight){
+        return price*numberOfNight + (int) getAdminFee(price);
     }
 }
+
